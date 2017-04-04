38f6be2c3009ddbe5eff6d0af014787cf8d6fb72
Joann <Lai.joann.cy@gmail.com>
2017-04-04 15:16:42 -0400

    Deleted comments

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ class App extends Component {
     super()
     this.state = {
       keyNum: 4,
-      currentUser: { name: "Bob" }, // optional. if currentUser is not defined, it means the user is Anonymous
+      currentUser: { name: "Bob" },
       messages: [
         {
           id: 1,
@@ -26,11 +26,8 @@ class App extends Component {
     console.log("componentDidMount <App />");
     setTimeout(() => {
       console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
       const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
       const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
       this.setState({messages: messages})
     }, 3000);
   }
